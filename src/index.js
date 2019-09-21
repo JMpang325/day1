c1dc10b187659b41bb63036ebd541eb6f0ed8e6b
Jimmy <1641894176@qq.com>
2019-09-21 11:59:35 +0800

    for loop

--- src/index.js
+++ src/index.js
@@ -5,6 +5,15 @@ var addNumbers = function(x,y){
 
 var testAddNumbers = addNumbers (4,4)
 // ===, <, >,<==, >==
-if( testAddNumbers > 10){
+
+if(( testAddNumbers > 10) && ( testAddnumbers < 15)){
     console.log(testAddNumbers)
+} else{
+    console.log("the number is incorrect")
 }
+
+for (ii=0; ii<= 100; ii=ii+1){
+    var current=ii 
+    console.log(current)
+
+} 
